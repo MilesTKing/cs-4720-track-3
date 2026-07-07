c7e3e3d3c3d71e08af852e4b3183422b472b0094
Miles King <kingmilest@gmail.com>
2026-07-06 23:47:52 -0400

    Updated Navbar Sizing.

--- project/app/NavBar.tsx
+++ project/app/NavBar.tsx
@@ -1,12 +1,12 @@
-'use client'
 import Link from 'next/link'
 export default function NavBar(){
     return (
         <>
-        <div className="flex basis-auto flex-column bg-black/80">
-            <Link href="/" className='flex'>Home</Link>
-            <Link href="./Profile.tsx" className='flex'>Profile</Link>
-            <Link href="./Friends.tsx" className='flex'>Friends</Link>
+        <div className="flex flex-row justify-center align-middle h-[3rem] ">
+            <Link href="/" className='flex-auto text-center text-[1.8rem]'><h1>Home</h1></Link>
+            <Link href="./friends" className='flex-auto w-auto text-center text-[1.8rem]' ><h1>Friends</h1></Link>
+            <Link href="./profile" className='flex-auto w-auto text-center text-[1.8rem]'><h1>Profile</h1></Link>
+            <button className='flex-auto bg-[gray] border-2 rounded-md text-[1.8rem] align-middle'>Sign Up</button>
         </div>
             </>
     )
